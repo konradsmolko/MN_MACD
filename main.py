eb37fbbc2ec15b6658389fa09ad3d64ed806d4c8
konrad <17689319+konradsmolko@users.noreply.github.com>
2019-03-10 00:01:49 +0100

    update

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
 import pandas as pd
 import jupyter as jp
 
+file = open("USDPLN_DAT.csv", "r")
+df = pd.read_csv(file, sep=';')
+del df["Volume"]
+del df["OPEN"]
+del df["HIGH"]
+del df["LOW"]
+df["DateTime"] = pd.to_datetime(df["DateTime"])
+df.set_index('DateTime', inplace=True)
+df.index.floor('1H')
+
+df.groupby(['DateTime']).mean()  # TODO
+
+print(df)
