be40a2e1e49d549d830375c78001a360a8cdade6
konrad <17689319+konradsmolko@users.noreply.github.com>
2019-03-10 23:13:16 +0100

    data operations, plotting

--- main.py
+++ main.py
@@ -1,59 +1,129 @@
 import pandas as pd
-import jupyter as jp
+import plotly as py
+import plotly.graph_objs as go
 
 # Constants for fine tuning
-FILENAME = "USDPLN_DAT.csv"
-INTERVAL = '6H'
+MODE = 'OFFLINE'            # 'OFFLINE' or 'ONLINE'
+MULTIPLIER = 100            # Multiplier for MACD and SIGNAL plots, for visibility reasons. Set to 1 to ignore.
+
+if MODE == 'ONLINE':
+    FILENAME = "https://raw.githubusercontent.com/plotly/datasets/master/finance-charts-apple.csv"
+    DATA_COLUMN = 'AAPL.Close'
+    DATE_COLUMN = 'Date'
+    INTERVAL = 'none'
+else:
+    FILENAME = "DAT_ASCII_USDPLN_M1_2018.csv"
+    DATA_COLUMN = 'Bar CLOSE Bid Quote'
+    DATE_COLUMN = 'DateTime Stamp'
+    INTERVAL = '6H'  # set to 'none' to disable interval grouping and rounding of values
+
+
+# Function for calculating EMA_n
+def calculate_ema(df, n, today, column):
+    if n <= 2:
+        return 0
+
+    if not isinstance(df, pd.DataFrame):
+        raise TypeError
+
+    one_minus_alpha = 1 - (2 / (n - 1))
+    """
+    ema = (data[0] + data[i]*one_minus_alpha^i + ... + data[n]*one_minus_alpha^n)
+    / (1 + one_minus_alpha^i + ... + one_minus_alpha^n)
+    """
+    apow = 1
+    topdiv = df.iloc[today][column]
+    botdiv = apow
+    for i in range(1, n):
+        # apow = pow(one_minus_alpha, i)
+        apow = apow * one_minus_alpha  # Should be less calculation intensive than pow()
+        topdiv = topdiv + df.iloc[today - i][column] * apow
+        botdiv = botdiv + apow
+
+    return topdiv / botdiv
+
 
 # Preparing the file
 sourceDF = pd.read_csv(FILENAME, sep=';')
-del sourceDF["Volume"]
-del sourceDF["OPEN"]
-del sourceDF["HIGH"]
-del sourceDF["LOW"]
-sourceDF["DateTime"] = pd.to_datetime(sourceDF["DateTime"])
-sourceDF.set_index('DateTime', inplace=True)
+sourceDF[DATE_COLUMN] = pd.to_datetime(sourceDF[DATE_COLUMN])
+sourceDF.set_index(DATE_COLUMN, inplace=True)
 
-# Reducing the time intervals to 6H with averaging the values in each hour, for a total of 1092 entries
-sourceDF = sourceDF.groupby(sourceDF.index.floor(INTERVAL)).mean()
-totalCount = sourceDF.count()['CLOSE']
+# Reducing the time intervals to INTERVAL with averaging the values
+if INTERVAL != 'none':
+    sourceDF = sourceDF.groupby(sourceDF.index.floor(INTERVAL)).mean()
 
-# TODO: Calculate EMA
+totalCount = sourceDF.count()[DATA_COLUMN]
+
+# Calculating MACD, EMA_12, EMA_26
 # MACD = EMA_12 - EMA_26
-ema = []
-for day in range(0, 26):
-    # ema.append()
-    pass
+macd = []
+ema_12 = []
+ema_26 = []
+for day in range(0, 12):
+    ema = calculate_ema(sourceDF, day, day, DATA_COLUMN)
+    macd.append(ema)
+    ema_12.append(ema)
+    ema_26.append(ema)
+
+for day in range(12, 26):
+    ema_12_ = calculate_ema(sourceDF, 12, day, DATA_COLUMN)
+    ema_26_ = calculate_ema(sourceDF, day, day, DATA_COLUMN)
+    macd.append(ema_12_ - ema_26_)
+    ema_12.append(ema_12_)
+    ema_26.append(ema_26_)
+
 for day in range(26, totalCount):
-    print(day)
+    ema_12_ = calculate_ema(sourceDF, 12, day, DATA_COLUMN)
+    ema_26_ = calculate_ema(sourceDF, 26, day, DATA_COLUMN)
+    macd.append(ema_12_ - ema_26_)
+    ema_12.append(ema_12_)
+    ema_26.append(ema_26_)
 
-# TODO: Calculate MACD
+# Add EMA12, EMA26, MACD to sourceDF
+sourceDF['EMA_12'] = pd.Series(ema_12, index=sourceDF.index)
+sourceDF['EMA_26'] = pd.Series(ema_26, index=sourceDF.index)
+sourceDF['MACD'] = pd.Series(macd, index=sourceDF.index)
 
-# TODO: Calculate Signal
+# Calculating Signal
+signal = []
+for day in range(0, 9):
+    ema = calculate_ema(sourceDF, day, day, 'MACD')
+    signal.append(ema)
+for day in range(9, totalCount):
+    ema = calculate_ema(sourceDF, 9, day, 'MACD')
+    signal.append(ema)
 
-# TODO: Add EMA, MACD, Signal to sourceDF and display Signal and MACD on the same graph
-sourceDF['EMA12'] = 0
-sourceDF['EMA26'] = 0
-sourceDF['MACD'] = 0
-sourceDF['SIGNAL'] = 0
+sourceDF['SIGNAL'] = pd.Series(signal, index=sourceDF.index)
+
+# I have to cut off the first 20 intervals due to odd values
+sourceDF = sourceDF.iloc[20:, ]
+
+# And this is where we enter the plotly part.
+# Preparing and displaying the graph, MACD and SIGNAL * 100 for visibility against raw data
+raw = go.Scatter(
+    name='RAW VALUE',
+    x=sourceDF.index,
+    y=sourceDF[DATA_COLUMN]
+)
+macd = go.Scatter(
+    name='MACD',
+    x=sourceDF.index,
+    y=sourceDF['MACD'] * MULTIPLIER
+)
+signal = go.Scatter(
+    name='SIGNAL',
+    x=sourceDF.index,
+    y=sourceDF['SIGNAL'] * MULTIPLIER
+)
+data_raw = [raw]
+data_macd = [macd, signal]
+data_mixed = [macd, signal, raw]
+py.offline.plot(data_raw, filename='raw.html')
+py.offline.plot(data_macd, filename='macd.html')
+py.offline.plot(data_mixed, filename='mixed.html')
+
+# 50% of the project points is all above this comment.
 
 # TODO: Figure out how to get buy/sell triggers and display them on the graph
 
 # TODO: Create a bot that will "trade" for the year using the triggers
-
-
-def calculate_ema(data, n):
-    if n <= 1:
-        return 0
-
-    one_minus_alpha = 1 - (2 / (n - 1))
-    # ema = (data[0] + data[i]*one_minus_alpha^i + ... + data[n]*one_minus_alpha^n)
-    # / (1 + one_minus_alpha^1 + ... + one_minus_alpha^n)
-    topdiv = 0
-    botdiv = 0
-    for i in range(n):
-        apow = pow(one_minus_alpha, i)
-        topdiv = topdiv + data[i]*apow
-        botdiv = botdiv + apow
-
-    return topdiv / botdiv
